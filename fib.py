21a93fca3e04e984d66c02899878c3e49991d584
sasaki.h1026@gmail.com <yoyogogo1026>
2020-05-16 18:12:53 +0900

    memory使用量を可視化できるように修正

--- fib.py
+++ fib.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- Coding: utf-8 -*-
 import time
+from memory_profiler import profile
 
 def Fib(n):
     if n == 1:
@@ -10,8 +11,12 @@ def Fib(n):
     else:
         return Fib(n - 1)+Fib(n - 2)
 
-if __name__ == '__main__':
+@profile
+def main():
     start = time.time()
-    print([Fib(n) for n in range(1, 45)])
+    print([Fib(n) for n in range(1, 10)])
     result_time = time.time() - start
     print ("result_time: {0}".format(result_time) + " sec")
+
+if __name__ == '__main__':
+    main()
